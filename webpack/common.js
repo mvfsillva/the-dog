27973c78efc95a4a4c5df103abe7ed9c3430e37e
Marcus Silva <mvfsillva@gmail.com>
2018-07-18 13:02:40 -0300

    Create common config

---
+++ webpack/common.js
@@ -0,0 +1,8 @@
+const { join } = require('path')
+
+const paths = {
+  root: join(__dirname, '..'),
+  src: join(__dirname, '..', 'src'),
+  public: join(__dirname, '..', 'public'),
+  build: join(__dirname, '..', 'build')
+}
