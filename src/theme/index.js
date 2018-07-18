1eeb4cb45b8443ed87e2e4c1b64cccd55b18cf90
Marcus Silva <mvfsillva@gmail.com>
2018-07-18 19:23:08 -0300

    Create theme

---
+++ src/theme/index.js
@@ -0,0 +1,19 @@
+const baselineGrid = 4
+const interfaceGrid = 8
+
+const breakpoints = ['576px', '768px', '992px', '1200px']
+
+const colors = {
+  primary: {
+    default: '#6800c3',
+    fallback: '#a55cff',
+    text: '#fff',
+  },
+}
+
+export default {
+  baselineGrid,
+  interfaceGrid,
+  breakpoints,
+  colors,
+}
