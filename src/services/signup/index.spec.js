42d96cdb5ba2e5919d5ab4456e5f6f0e8c9be15c
Marcus Silva <mvfsillva@gmail.com>
2018-07-19 04:20:34 -0300

    Create signup spec

---
+++ src/services/signup/index.spec.js
@@ -0,0 +1,22 @@
+import nock from 'nock'
+import config from '../config'
+import userService from '.'
+
+describe('[User Service]', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  const mockResponse = (error, status, body) => {
+    const req = nock(config.api.baseUrl).post(config.user.signup, _ => true)
+    return error ? req.replyWithError(error) : req.reply(status, body)
+  }
+
+  it('should resolves with token', async () => {
+    const respose = { token: 'abcdefg' }
+    mockResponse(null, 200, respose)
+    const user = await userService.login('email@client.com')
+
+    expect(user).toEqual(respose)
+  })
+})
