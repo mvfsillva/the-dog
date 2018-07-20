2ccc894edd828dd5d50751321654988c75a236d2
Marcus Silva <mvfsillva@gmail.com>
2018-07-20 19:10:51 -0300

    Create component TextField

---
+++ src/components/TextField/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { transitions } from 'polished'
+import { transition } from '../../mixins/style.transtion'
+import If from 'components/common/If'
+
+const StyledInput = styled.input`
+  background-color: transparent;
+  border: 0;
+  padding: 3px 0;
+  width: 100%;
+  margin: 5px;
+  border-bottom: 1px solid ${props => props.theme.colors.gray['300']};
+  outline: snow;
+
+  &:hover {
+    border-bottom-color: ${props => props.theme.colors.gray['500']};
+  }
+
+  &:focus {
+    border-bottom-color: ${props => props.theme.colors.primary.default};
+  }
+
+  &::placeholder {
+    color: ${props => props.theme.colors.gray['600']};
+  }
+
+  ${transitions(
+    transition({ property: 'border-color' }),
+    transition({ property: 'padding-left' }),
+  )};
+`
+
+const StyledLabel = styled.label`
+  color: ${props => props.theme.colors.gray['500']};
+  font-size: 1rem;
+  line-height: 2em;
+  margin: 5px;
+`
+
+const Wrapper = styled.div`
+  margin-bottom: 1rem;
+  label {
+    vertical-align: middle;
+  }
+`
+
+const TextField = ({ name, label, type, ...props }) => {
+  return (
+    <Wrapper>
+      <If test={label}>
+        <StyledLabel htmlFor={name}>{label}</StyledLabel>
+      </If>
+      <StyledInput id={name} name={name} type={type} {...props} />
+    </Wrapper>
+  )
+}
+
+TextField.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+}
+
+export default TextField
