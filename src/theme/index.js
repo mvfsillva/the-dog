a40967b626d0d4991d38978eefdf57cb49555809
Marcus Silva <mvfsillva@gmail.com>
2018-07-20 19:12:54 -0300

    Add new colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -5,9 +5,27 @@ const breakpoints = ['576px', '768px', '992px', '1200px']
 
 const colors = {
   primary: {
-    default: '#6800c3',
-    fallback: '#a55cff',
-    text: '#fff',
+    gray: '#7F7F7F',
+    mercury: '#E4E4E4',
+    silver: '#C2C1C1',
+  },
+
+  gray: {
+    default: '#7e7e7e',
+    light: '#bfbfbf',
+    ultraLight: '#f7f7f7',
+
+    // scale
+    '050': '#fafafa',
+    100: '#f5f5f5',
+    200: '#eee',
+    300: '#e0e0e0',
+    400: '#bdbdbd',
+    500: '#9e9e9e',
+    600: '#757575',
+    700: '#616161',
+    800: '#424242',
+    900: '#212121',
   },
 }
 
