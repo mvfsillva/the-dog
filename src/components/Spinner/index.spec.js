1088b631c807284371eba72031b5d47ffc597758
Marcus Silva <mvfsillva@gmail.com>
2018-07-21 02:52:19 -0300

    Fix linter

--- src/components/Spinner/index.spec.js
+++ src/components/Spinner/index.spec.js
@@ -7,12 +7,18 @@ const LOTTIE_CLASSNAME = '_test--lottie'
 beforeEach(() => {
   jest.unmock('react-lottie')
   const lottie = require.requireActual('react-lottie')
-  lottie.default = props => (<div className={LOTTIE_CLASSNAME} {...props} />)
+  lottie.default = props => <div className={LOTTIE_CLASSNAME} {...props} />
 })
 
 describe('[Component: Spinner]', () => {
   it('Renders loading animation', () => {
-    const [width, height, loop, autoplay, animation] = [100, 200, true, false, {layers: [{ name: 'Chicken Black Power' }]}]
+    const [width, height, loop, autoplay, animation] = [
+      100,
+      200,
+      true,
+      false,
+      { layers: [{ name: 'Chicken Black Power' }] },
+    ]
     const spinner = mount(
       <Spinner
         width={width}
@@ -20,7 +26,7 @@ describe('[Component: Spinner]', () => {
         loop={loop}
         autoplay={autoplay}
         animation={animation}
-      />
+      />,
     )
 
     const lottieProps = spinner.find(`.${LOTTIE_CLASSNAME}`).props()
