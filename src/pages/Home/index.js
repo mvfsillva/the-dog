913e8f3218d68fa0f8f4aad5c260b73796603c9c
Marcus Silva <mvfsillva@gmail.com>
2018-07-22 01:38:21 -0300

    Add content for layout improvement

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Header from 'components/Header'
+import Navigation from 'components/Navigation'
 import Signup from 'templates/Signup'
 
 const Wrapper = styled.div`
@@ -9,15 +10,27 @@ const Wrapper = styled.div`
   padding-top: 3.75rem;
   min-height: 100vh;
   box-sizing: border-box;
+  justify-content: center;
+  align-self: center;
   font-family: ${props => props.theme.fonts.primary};
   @media screen and (max-width: 640px) {
     padding-top: 3.25rem;
   }
 `
 
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-bottom: 3rem;
+`
+
 const Home = () => (
   <Wrapper>
-    <Header headline="the iddog" uppercase />
+    <Content>
+      <Header headline="the iddog" uppercase />
+      <Navigation />
+    </Content>
     <Signup />
   </Wrapper>
 )
