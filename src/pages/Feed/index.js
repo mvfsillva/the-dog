98e6f1d15b16b2a58ea5773fce04653be643d77e
Marcus Silva <mvfsillva@gmail.com>
2018-07-22 20:57:35 -0300

    Create Feed Page

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { Component } from 'React'
 
-const Feed = () => <h1>Feed Page!!</h1>
+import Header from '@components/Header'
+import Navigation from '@components/Navigation'
+
+import { HeaderWrapper } from '~/styles/Wrapper'
+import Content from '~/styles/Content'
+
+class Feed extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        <Content>
+          <Header headline="the iddog" uppercase />
+          <Navigation />
+        </Content>
+      </HeaderWrapper>
+    )
+  }
+}
 
 export default Feed
