deea7c9084f83ee12d86e1f1a5f689f6b9f15b85
Marcus Silva <mvfsillva@gmail.com>
2018-07-23 07:11:14 -0300

    Add react-lazyload

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,11 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import tag from 'clean-tag'
+import LazyLoad from 'react-lazyload'
+import { CSSTransitionGroup } from 'react-transition-group'
+
+import { transitions } from 'polished'
+import { transition } from '@mixins/transition'
 
 const Panel = styled(tag).attrs({ bg: 'white' })`
   border: solid 1px #e9eef0;
@@ -12,9 +17,18 @@ const Panel = styled(tag).attrs({ bg: 'white' })`
   height: 360px;
   padding: 10px;
   margin: 10px 0 0 10px;
-  > img {
+  img {
     width: 320px;
     height: 360px;
+  }
+
+  .fade-appear {
+    opacity: 0.01;
+  }
+
+  .fade-appear.fade-appear-active {
+    ${transitions(transition({ property: 'opacity', duration: '2s' }))};
+    opacity: 1;
   }
 `
 
@@ -39,7 +53,18 @@ const Card = ({ data }) => (
   <Container>
     {data.map(dog => (
       <Panel key={formatId(dog)}>
-        <img src={dog} />
+        <LazyLoad throttle={200} height={300}>
+          <CSSTransitionGroup
+            key="1"
+            transitionName="fade"
+            transitionAppear
+            transitionAppearTimeout={600}
+            transitionEnter={false}
+            transitionLeave={false}
+          >
+            <img src={dog} />
+          </CSSTransitionGroup>
+        </LazyLoad>
       </Panel>
     ))}
   </Container>
